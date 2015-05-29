419d8efdbe23394d644b0496f93c8fd567d5f97b
Steve <steve.schneider99@gmail.com>
2015-05-29 09:52:54 -0700

    add first spec and initial code for html page, scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+var findWord = function(word) {
+
+};
+
+
+
+
+
+
+
+
+$(document).ready(function() {
+  $('form#initial_input').submit(function(event) {
+    var input_string = ($('input#input_string').val());
+
+
+  });
+
+});
---
+++ spec/specs.js
@@ -0,0 +1,6 @@
+describe('findWord', function() {
+  it('checks to see if two words are the same', function() {
+    expect("apple".findWord("apple")).to.equal(true)
+  });
+
+});
