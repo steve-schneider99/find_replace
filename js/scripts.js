275fe76e21414adaa0fde70ccc8a7f20d6ef9901
Steve <steve.schneider99@gmail.com>
2015-05-29 12:01:22 -0700

    code closer to working now. Form redid to have all paramaters entered at once. returned sentence has commas and no spaces, even with join used properly.

--- js/scripts.js
+++ js/scripts.js
@@ -6,39 +6,26 @@ var findWord = function(word1, word2) {
   }
 };
 
-var replaceWord = function(original, replacement) {
-  var inputArray = inputString.split(" ")
+var replaceWord = function(sentence, original, replacement) {
+  inputArray = sentence.split(" ");
   inputArray.forEach(function(word, index) {
     if (original === word) {
       inputArray[index] = replacement;
-    } else {
-
-    }
-  });
+  } else {
+  }
+});
   inputArray.join(" ");
   return inputArray;
 }
 
-
-
-
-
-
 $(document).ready(function() {
   $('form#find_replace').submit(function(event) {
+    var inputString = ($('input#input_string').val());
     var targetWord = ($('input#target_word').val());
     var newWord = ($('input#new_word').val());
-    var result = replaceWord(targetWord, newWord);
-
-    debugger;
-
-
-    $('#user_sentence').show()
+    var result = replaceWord(inputString, targetWord, newWord);
+    $('.new_sentence').text(result);
     $('#replace_result').show();
     event.preventDefault();
   });
-
-
-
-
 });
